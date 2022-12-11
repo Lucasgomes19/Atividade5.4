e1de606d4614497beed62149b53117462b03b985
Lucasgomes19 <lucasmadrid01@gmail.com>
2022-12-10 22:42:10 -0300

    Atividade4

---
+++ atv4.java
@@ -0,0 +1,16 @@
+public class atv4 {
+
+    public static void main(String[] args) {
+
+        if(tipoNumero(10) < 0) {
+            System.out.println("N");
+        }
+        else {
+            System.out.println("P");
+        }
+    }
+
+    static int tipoNumero(int n) {
+        return n;
+    }
+}
